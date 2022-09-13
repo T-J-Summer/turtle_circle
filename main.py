0f299b699f5207bc47e9cec22bea1c3a1a358c8c
Theo Summerfield <113345547+T-J-Summer@users.noreply.github.com>
2022-09-13 15:52:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+# Run the code to see the shape it draws. Modify it to make your own pattern. You can use pentagons and hexagons that we looked at earlier.
+
+
+import turtle # imports the module so we can use its methods
+my_turtle=turtle.Turtle() # create a Turtle named my_turtle
+my_turtle.speed(100)
+my_turtle.color('red')
+
+for i in range(0,36):
+  for i in range(0,5):
+    my_turtle.color('red')
+    my_turtle.forward(100)
+    my_turtle.right(72)
+  my_turtle.color('blue')
+  my_turtle.circle(70)
+  my_turtle.color('red')
+  my_turtle.right(10)
+  my_turtle.color('green')
+  for i in range(0,4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+  my_turtle.color('yellow')
+  my_turtle.circle(80)
+my_turtle.forward(350)
+for i in range(0,36):
+  for i in range(0,5):
+    my_turtle.color('red')
+    my_turtle.forward(100)
+    my_turtle.right(72)
+  my_turtle.color('blue')
+  my_turtle.circle(70)
+  my_turtle.color('red')
+  my_turtle.right(10)
+  my_turtle.color('green')
+  for i in range(0,4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+  my_turtle.color('yellow')
+  my_turtle.circle(80)
+my_turtle.right(180)
+my_turtle.forward(700)
+for i in range(0,36):
+  for i in range(0,5):
+    my_turtle.color('red')
+    my_turtle.forward(100)
+    my_turtle.right(72)
+  my_turtle.color('blue')
+  my_turtle.circle(70)
+  my_turtle.color('red')
+  my_turtle.right(10)
+  my_turtle.color('green')
+  for i in range(0,4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+  my_turtle.color('yellow')
+  my_turtle.circle(80)
+my_turtle.right(180)
+my_turtle.forward(350)
+my_turtle.right(90)
+my_turtle.forward(350)
+for i in range(0,36):
+  for i in range(0,5):
+    my_turtle.color('red')
+    my_turtle.forward(100)
+    my_turtle.right(72)
+  my_turtle.color('blue')
+  my_turtle.circle(70)
+  my_turtle.color('red')
+  my_turtle.right(10)
+  my_turtle.color('green')
+  for i in range(0,4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+  my_turtle.color('yellow')
+  my_turtle.circle(80)
+my_turtle.right(180)
+my_turtle.forward(700)
+for i in range(0,36):
+  for i in range(0,5):
+    my_turtle.color('red')
+    my_turtle.forward(100)
+    my_turtle.right(72)
+  my_turtle.color('blue')
+  my_turtle.circle(70)
+  my_turtle.color('red')
+  my_turtle.right(10)
+  my_turtle.color('green')
+  for i in range(0,4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+  my_turtle.color('yellow')
+  my_turtle.circle(80)
+  
+
+
